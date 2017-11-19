23f2fb3950005d260dcc00ff2add5d7ac3b9f02c
kelseyde <daniel.kelsey@hotmail.co.uk>
2017-11-19 20:05:23 +0000

    working web scraper with url-based article titles

--- server/server.js
+++ server/server.js
@@ -12,20 +12,67 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
-var url = "https://plato.stanford.edu/contents.html"
-
-
-request(url, function(error, response, html) {
-  const $ = cheerio.load(html);
-  var json = []
-  $("li", "#content").each(function() {
-    var data = $(this);
-    var url = "https://plato.stanford.edu/" + data.children().first().attr("href");
-    var title = data.children().first().children().first().text();
-    var obj = {title: title, url: url}
-    json.push(obj);
+var getEncyclopediaArticles = function(url) {
+  var articles = [];
+  var filteredArticles = [];
+  request(url, function(error, response, html) {
+    var $ = cheerio.load(html);
+    $("li", "#content").each(function() {
+      var data = $(this);
+      var url = "https://plato.stanford.edu/" + data.children().first().attr("href");
+      var title = url.substring(35).slice(0, -1);
+      var article = {title: title, url: url}
+      articles.push(article);
+    });
+    filteredArticles = articles.filter(function(article) {
+      return !(article.url.includes("undefined"))
+          && !(article.title === "");
+    });
   });
-  console.log(json);
-})
+}
+
+var articles = getEncyclopediaArticles("https://plato.stanford.edu/contents.html");
+console.log(articles);
+
+
+
+
+// var getUrls = function(url) {
+//   var json = [];
+//   request(url, function(error, response, html) {
+//     var $ = cheerio.load(html);
+//     $("li", "#content").each(function() {
+//       var data = $(this);
+//       var url = "https://plato.stanford.edu/" + data.children().first().attr("href");
+//       var title;
+//       var obj = {title: "", url: url}
+//       console.log(obj);
+//       json.push(obj);
+//     });
+//   });
+//   console.log(json);
+//   return json;
+// }
+//
+// var getTitles = function(json) {
+//   for (obj of json) {
+//     var url = obj.url;
+//     request(url, function(error, response, html) {
+//       obj.title = title;
+//     });
+//   }
+//   console.log(json);
+// }
+//
+// var jsonWithUrls = getUrls("https://plato.stanford.edu/contents.html");
+// console.log(jsonWithUrls);
+// getTitles(jsonWithUrls);
+
+
+// request(url, function(error, response, html) {
+//   $ = cheerio.load(html);
+//   title = $("h1", "#aueditable");
+//   console.log(title);
+// })
 
 app.use(express.static(__dirname + '/../client/build'))
